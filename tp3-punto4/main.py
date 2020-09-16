15c30c57988476a5a0eed5d23ea633b394e3784f
LuluAllende19 <abigail_lucii@hotmail.com>
2020-09-16 19:58:43 -0300

    Siguiendo con la transformacion de datos

--- tp3-punto4/main.py
+++ tp3-punto4/main.py
@@ -10,6 +10,24 @@ def transformar_datos(dataframe):
                               'osm_admin_level_4': 'provincia',
                               'osm_admin_level_8': 'ciudad',
                               'nue_casosconf_diff': 'nue_casosconf'}, inplace=True)
+
+    # los datos iniciales fueron antes de comenzar la cuarentena y no tienen un dia asociado
+    dataframe['dia_cuarentena'] = dataframe['dia_cuarentena'].fillna(0)
+
+    # Son pocas ciuaddes las que estan identificadas, pero es un dato importante
+    dataframe['ciudad'] = dataframe['ciudad'].fillna('C_Indeterminado')
+
+    # despues de cierto tiempo, el virus se extendio y se presupone que el contagio siempre es por transmision comunitaria
+    dataframe['transmision_tipo'] = dataframe['transmision_tipo'].fillna(
+        'transmision comunitaria')
+
+    # despues de cierto tiempo estos datos se obtuvieron de informes pronviciales
+    dataframe['informe_tipo'] = dataframe['informe_tipo'].fillna(
+        'informe provincial')
+
+    # es necesario pasar esta columna a enteros, por algun motivo la transformacion anterior lo deja como flotante y ese tipo de dato no es representatitvo
+    dataframe['dia_cuarentena'] = dataframe['dia_cuarentena'].astype('int64')
+
     print(dataframe.head(5))
     dataframe.to_csv('datos/transformacion.csv', index=False)
 
